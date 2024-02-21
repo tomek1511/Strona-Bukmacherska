f5f4b055d78155edcd7adf42ea85e5fc988f75fb
tomek1511 <74348221+tomek1511@users.noreply.github.com>
2024-02-21 09:32:44 +0100

    Add files via upload

---
+++ wyniki.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="./style/main.css">
+    <title> Strona Bukmacherska</title>
+</head>
+<body>
+    <header>
+        <h1>Strona Bukmacherska</h1>
+    </header>
+
+    <nav>
+        <a href="./index.html">Strona główna</a> 
+        <a href="./zaklady.html">Zakłady</a> 
+        <a href="./wyniki.html">Wyniki</a> 
+        <a href="./kontakt.html">Kontakt</a>
+    </nav>
+</body>
+</html>
